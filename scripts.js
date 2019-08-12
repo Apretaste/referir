c9f83b750ae5625b59e09e0854eee7a934ddd45f
Ricardo <ricardo@apretaste.org>
2019-08-12 15:00:22 -0500

    Back to black, ES5 rewrite

--- scripts.js
+++ scripts.js
@@ -1,46 +1,55 @@
+"use strict";
+
 function showToast(text) {
-  M.toast({html: text});
+  M.toast({
+    html: text
+  });
 }
 
 function send() {
-	// get the values
-	var checked = $('#check').prop('checked')
-	var username = $('#username').val();
+  // get the values
+  var checked = $('#check').prop('checked');
+  var username = $('#username').val(); // check username is not empty
 
-	// check username is not empty
-	if( !checked && !username) {
+  if (!checked && !username) {
     showToast('Díganos quien le refirió');
-		return false;
-	}
+    return false;
+  } // send the request
 
-	// send the request
-	apretaste.send({
-		command: "REFERIR AMIGO", 
-		data: {username: username},
-		redirect: true});
+
+  apretaste.send({
+    command: "REFERIR AMIGO",
+    data: {
+      username: username
+    },
+    redirect: true
+  });
 }
 
 function sendInvitation() {
-	var email = $('#email').val();
-	if(isEmail(email)){
-		apretaste.send({
-			'command': 'REFERIR INVITAR',
-			'data': {'email':email}
-		})
-	}
-	else showToast("Ingrese un email válido");
+  var email = $('#email').val();
+
+  if (isEmail(email)) {
+    apretaste.send({
+      'command': 'REFERIR INVITAR',
+      'data': {
+        'email': email
+      }
+    });
+  } else showToast("Ingrese un email válido");
 }
 
 function toggle() {
-	if($('#check').prop('checked')) $('#input').slideUp()
-	else $('#input').slideDown()
+  if ($('#check').prop('checked')) $('#input').slideUp();else $('#input').slideDown();
 }
 
 function isEmail(email) {
-	var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-	if(!regex.test(email)) {
-		return false;
-	}else{
-		return true;
-	}
+  var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+  if (!regex.test(email)) {
+    return false;
+  } else {
+    return true;
+  }
 }
+
